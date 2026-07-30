df9f1affbbeef5243868f4b39e0bd9d4a03e816f
AcquahMichelle <acquahmichelle91@gmail.com>
2026-07-30 19:46:09 +0100

    Implement student grade system

--- assignment_01_prime_checker.cpp
+++ assignment_01_prime_checker.cpp
@@ -33,7 +33,6 @@
 // =============================================================================
 // YOUR CODE BELOW — remove the // symbols from the scaffold and fill it in
 // =============================================================================
-
 #include <iostream>
 using namespace std;
 
@@ -41,23 +40,23 @@ bool isPrime(int num) {
     if (num < 2) {
         return false;
     }
-    for (int i = 2; i < num; i++) {
-       if (num % i== 0) {
-        return false;
-       }
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            return false;
+        }
     }
     return true;
 }
+
 int main() {
     int number;
-
     cout << "Enter a number: ";
     cin >> number;
 
     if (isPrime(number)) {
-        cout << number << " is a prime number ." << endl;
+        cout << number << " is a prime number." << endl;
     } else {
-        cout << number << " is NOT a prime number ." << endl;
+        cout << number << " is NOT a prime number." << endl;
     }
 
     return 0;
